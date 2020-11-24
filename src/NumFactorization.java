7d7812cb7a85a1c922cb85c7ee34c73fdc7553da
smirnyagina-vn <veronika.smirnyagina@gmail.com>
2020-11-24 21:14:31 +0300

    start

---
+++ src/NumFactorization.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class NumFactorization {
+
+    public NumFactorization()
+    {
+
+    }
+
+    public static void main(String[] args) {
+        NumFactorization numFactorization = new NumFactorization();
+    }
+
+    public void PollardFactorization(BigInteger num)
+    {
+
+    }
+}
